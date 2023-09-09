4a67707c4dbd4ca10b055acb5d93dc0d046fcfb5
Kai Quan <S10241963@connect.np.edu.sg>
2023-09-09 18:36:22 +0800

    Add project files.

---
+++ Leaderboard System (Dell)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Leaderboard_System__Dell_.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
