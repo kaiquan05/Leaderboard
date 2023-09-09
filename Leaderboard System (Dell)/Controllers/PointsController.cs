25596ce60b3b436aba3e87df0b5aa0c6ef156da3
Kai Quan <S10241963@connect.np.edu.sg>
2023-09-10 02:49:43 +0800

    dynamic leaderboard (5 min refresh)

--- Leaderboard System (Dell)/Controllers/PointsController.cs
+++ Leaderboard System (Dell)/Controllers/PointsController.cs
@@ -2,17 +2,40 @@
 using Leaderboard_System__Dell_.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Timers;
 
 namespace Leaderboard_System__Dell_.Controllers
 {
 	public class PointsController : Controller
 	{
 		PointsDAL pointsContext = new PointsDAL();
+		private System.Timers.Timer refreshTimer;
+		private List<Points> cachedPoints = new List<Points>();
+
+		public PointsController()
+		{
+			// Create a timer and set the interval to 5 minutes
+			refreshTimer = new System.Timers.Timer(3000);
+			refreshTimer.Elapsed += RefreshData;
+			refreshTimer.AutoReset = true; // Enable periodic triggering
+			refreshTimer.Start(); // Start the timer
+
+			// Load the initial data
+			cachedPoints = pointsContext.AssignLeaderboard();
+		}
+
 		// GET: PointsController
 		public ActionResult Index()
 		{
-			List<Points> points = pointsContext.AssignLeaderboard();
-			return View(points);
+			return View(cachedPoints);
+		}
+		private void RefreshData(object sender, ElapsedEventArgs e)
+		{
+			// This method will be called every 5 minutes
+			// Execute your SQL query here to refresh the data
+			List<Points> refreshedPoints = pointsContext.AssignLeaderboard();
+			// Update the cached data with the refreshed data
+			cachedPoints = refreshedPoints;
 		}
 
 		// GET: PointsController/Details/5
